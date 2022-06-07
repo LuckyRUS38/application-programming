42c395c0fa41a595a18e52138c719261dae54f6d
LuckyRUS38 <artemiykuzik@gmail.com>
2022-06-07 20:02:51 +0800

    Сделал скрипт по аналитике в ВК. Метод 6-ти рукопожатий, который надо доработать.

--- ПП/Bots/TelegramBot/cfg.py
+++
@@ -1,9 +0,0 @@
-token = '5205753309:AAG-jnimF9zJADZEAJ8BnbL9FjG_ecRd8gk'
-
-db_pass = 'Lovemama1'
-db_host = '185.105.88.201'
-db_name = 'CatBot'
-db_user = 'bot'
-
-photos_folder = 'C:/Proggramming/repositories/programming/telegram_photos/'
-image_host = 'https://185.105.88.201.nip.io/telegram_photos/'
